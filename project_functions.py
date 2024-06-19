9d8c08bb1fa0a826f11a3074ef94ec82d5718f34
aliciaroberts <aliciaroberts@berkeley.edu>
2024-06-18 18:08:39 -0700

    made more functions!

--- project_functions.py
+++ project_functions.py
@@ -1,9 +1,19 @@
-# so im gonna build a library that can do all the lil things I need for my project: 
+# im gonna build a library that can do all the lil things I need for my project: 
 # find artist information
 # find song information
 # maybe some text analysis component 
 
-# need to install this first, so I'll put the steps here somewhere 
+# need to install this first, so I'll put the steps here somewhere
+# okay yeah so basically 1: go into your terminal or wherever you install
+# your packages. 
+# then 2: make sure that you set your path to the one that leads to PATH
+# this is for windows, so im not sure how it works on mac or linux, but for me
+# it was called PATH_Programs
+# 3: then do this command:
+# < py -m pip install lyricsgenius > <- py is for windows, python for mac
+# if it installs properly then you can inport the library fine and 
+# this code shouldn't error 
+
 import lyricsgenius
 
 # this is my personal token, so if you want to use this you'll have to 
@@ -42,7 +52,7 @@ def get_lyrics(art = None, song = None, s_ID = None):
         lyrics = (genius.search_song(title = song,
                     artist = art,
                     get_full_info = False, 
-                   ))
+                   ).lyrics)
     else:
         # this means nothing was submitted or the type of the 
         # variables is incorrect 
@@ -53,6 +63,50 @@ def get_lyrics(art = None, song = None, s_ID = None):
         if type(song) != str:
             raise Exception('Wrong data type for song title: looking for STRING')
     return lyrics 
+
+def get_release_date(song_dict, form = 'd'):
+    '''given a song dictionary, return the date the song came out.
+    if form == r, then return the readable option
+    otherwise return the dictionary version
+    OUTPUT: either INT or DICT'''
+    if form == 'd':
+        return song_dict['release_date_components'] # this one is better
+    if form == 'r':
+        return song_dict['release_date_for_display']
+    else:
+        raise Exception('Wrong form selected, please choose d for DICTIONARY or r for plain text')
+        
+
+def get_year(song):
+    '''given SONG, return the year the song came out
+    OUTPUT: INT'''
+    year = get_release_date(song)['year']
+    return year
+
+def get_artist_ID(artist):
+    '''given an artist, reaturn their artist ID 
+    OUTPUT: INT'''
+    ID = genius.search_artist(artist, max_songs = 0).id
+    return ID 
+
+
+def song_dictionary(artist_ID, song_title, most_popular_only = True):
+    '''returns the dictionary of keywords and information about a song
+    INPUTS: artist_ID: an integer value
+            song_title: the title of the song, not case sensitive
+    caveat: will return all songs of that artist with the same or similar title 
+    (eg the 10 minute version or Taylor's version vs the original, new features, etc. . . 
+    to account for this, the term: most_popular_only can be set to true to only return the most popular 
+    version of the song we want''' 
+    dictionary = (genius.search_artist_songs(artist_id = artist_ID, 
+                            search_term =song_title, 
+                                            sort = 'popular'))
+    if most_popular_only:
+        return dictionary['songs'][0]
+    return dictionary['songs']
+
+def get_song_ID(song):
+    return song['id']
 
 
 
